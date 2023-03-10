c91d74cd9bd3e9999b228219f30785b0f60c037c
Aamir Abbas <aamir@ogaamyr.com>
2023-03-10 04:02:19 +0000

    hotfix: renaming blogs.ejs to blog.ejs

--- 06 EJS/app.js
+++ 06 EJS/app.js
@@ -2,8 +2,22 @@
 const express = require('express');
 const app = express();
 
+const blogData = require("./blogData.json");
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+
+app.get("/", (req, res) => {
+  const blogs = blogData.blogs;
+  res.render("index", { blogs });
+});
+
+app.get("/blog/:id", (req, res) => {
+  const id = req.params.id;
+  const blog = blogData.blogs[id];
+  res.render("blog", { blog });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
